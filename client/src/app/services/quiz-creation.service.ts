a70078930db4e935244cd41dd4b20db1850254da
Rayan Ait Abdesselam <rayan.ait-abdesselam@polymtl.ca>
2023-09-12 22:15:13 -0400

    Réusinage : reonommer quizCreation à quiz-creation pour le service

---
+++ client/src/app/services/quiz-creation.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class QuizCreationService {
+    /*
+     * liste de questions
+     * removeQuestion()
+     * addQuestion()
+     * modifiyQuestion()
+     * saveQuestion() verifier si valide apres ca l'ajoute
+     * saveGame() verifie si game valide apres ca l'ajoute
+     * verifiyGameName verifiyGameDesc verifiyGameResponseTime
+     * swapQuestion(index 1, index 2)
+     * */
+}
