1e36d19f72138411930948a2eaa29a3287e34d0a
Rayan Ait Abdesselam <rayan.ait-abdesselam@polymtl.ca>
2023-09-12 23:12:33 -0400

    Fonction : ajout de variables de QuizCreationService

--- client/src/app/services/quiz-creation.service.ts
+++ client/src/app/services/quiz-creation.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
 
+interface FormChoice {
+    text: string;
+    isRight: boolean;
+    isValid: boolean;
+}
+
+interface FormQuestion {
+    text: string;
+    points: number;
+    choices: FormChoice[];
+    selectedChoices: number[];
+    isValid: boolean;
+}
+
 @Injectable({
     providedIn: 'root',
 })
 export class QuizCreationService {
+    gameTitle: string;
+    gameDuration: number;
+    gameDescription: string;
+    gameType: string;
+
+    gameQuestions : FormQuestion;
+
+
     /*
      * liste de questions
      * removeQuestion()
