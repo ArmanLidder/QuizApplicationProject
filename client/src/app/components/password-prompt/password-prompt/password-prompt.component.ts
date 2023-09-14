9445e4c838bb3af1f4e2fbdf5fce65abcdcb4edf
hb.96.montreal <hb.96.montreal@gmail.com>
2023-09-14 18:46:44 -0400

    Add password-prompt component

---
+++ client/src/app/components/password-prompt/password-prompt/password-prompt.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-password-prompt',
+  templateUrl: './password-prompt.component.html',
+  styleUrls: ['./password-prompt.component.scss']
+})
+export class PasswordPromptComponent {
+
+}
