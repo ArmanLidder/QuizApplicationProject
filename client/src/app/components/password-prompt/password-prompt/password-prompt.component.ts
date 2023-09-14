9925d09d357a85b7d869ae211b7267587e1d5421
hb.96.montreal <hb.96.montreal@gmail.com>
2023-09-14 19:26:38 -0400

    Update password-prompt component: add HTML and Component logic

--- client/src/app/components/password-prompt/password-prompt/password-prompt.component.ts
+++ client/src/app/components/password-prompt/password-prompt/password-prompt.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-password-prompt',
-  templateUrl: './password-prompt.component.html',
-  styleUrls: ['./password-prompt.component.scss']
+    selector: 'app-password-prompt',
+    templateUrl: './password-prompt.component.html',
+    styleUrls: ['./password-prompt.component.scss'],
 })
 export class PasswordPromptComponent {
+    password: string = '';
+    loginStatus: string = '';
 
+    verifyPassword() {
+        return (this.loginStatus = this.password === 'admin' ? 'Login successful!' : 'Wrong password : Try again!');
+    }
 }
