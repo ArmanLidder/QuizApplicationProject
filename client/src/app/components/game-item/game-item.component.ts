901c24662e374d88929967712a6521e3b0719c73
hb.96.montreal <hb.96.montreal@gmail.com>
2023-09-15 14:42:33 -0400

    Update: game-item component

--- client/src/app/components/game-item/game-item.component.ts
+++ client/src/app/components/game-item/game-item.component.ts
@@ -1,24 +1,26 @@
 import { Component, Input } from '@angular/core';
-import { Quiz } from '@app/interfaces/quiz'; 
+import { Quiz } from '@app/interfaces/quiz';
 
 @Component({
     selector: 'app-game-item',
     templateUrl: './game-item.component.html',
     styleUrls: ['./game-item.component.scss'],
 })
-
 export class GameItemComponent {
     @Input() quiz: Quiz;
 
     deleteGame() {
+        //  TODO implement it with server
         return;
     }
 
     updateGame() {
+        //  TODO implement it with server
         return;
     }
 
     exportGame() {
+        // TODO implement it with server
         return;
     }
 }
