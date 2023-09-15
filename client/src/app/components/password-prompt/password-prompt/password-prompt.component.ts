f22b9f46357329fb74b8007b1b9ae245c5a1344c
hb.96.montreal <hb.96.montreal@gmail.com>
2023-09-14 21:06:03 -0400

    Update password-prompt: Adding feedback on users actions

--- client/src/app/components/password-prompt/password-prompt/password-prompt.component.ts
+++ client/src/app/components/password-prompt/password-prompt/password-prompt.component.ts
@@ -8,8 +8,42 @@ import { Component } from '@angular/core';
 export class PasswordPromptComponent {
     password: string = '';
     loginStatus: string = '';
+    inputBorderColor: string = '';
+    textColor: string = '';
 
-    verifyPassword() {
-        return (this.loginStatus = this.password === 'admin' ? 'Login successful!' : 'Wrong password : Try again!');
+    isPasswordGood() {
+        return this.password === 'admin';
+    }
+
+    // For now this method enable a preview of the component but enventually we will have to change server validation
+    validatePassword() {
+        return (this.loginStatus = this.isPasswordGood() ? 'Login successful!' : 'Wrong password : Try again!');
+    }
+
+    isLoginSuccessful() {
+        return this.loginStatus === 'Login successful!';
+    }
+
+    updateBorderColor() {
+        return (this.inputBorderColor = this.isLoginSuccessful() ? 'green-border' : 'red-border');
+    }
+
+    updateTextColor() {
+        return (this.textColor = this.isLoginSuccessful() ? 'green-text' : 'red-text');
+    }
+
+    resetInput() {
+        if (!this.isLoginSuccessful()) this.password = '';
+    }
+
+    updateStyle() {
+        this.updateBorderColor();
+        this.updateTextColor();
+        this.resetInput();
+    }
+
+    authentificateAdmin() {
+        this.validatePassword();
+        this.updateStyle();
     }
 }
