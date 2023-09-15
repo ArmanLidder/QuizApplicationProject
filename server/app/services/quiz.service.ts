383f98dcfae1b2e18857d41472295eaff2c09efc
Rayan Ait Abdesselam <rayan.ait-abdesselam@polymtl.ca>
2023-09-15 10:44:15 -0400

    Feature: implemented quiz service funcitons

---
+++ server/app/services/quiz.service.ts
@@ -0,0 +1,34 @@
+import { Service } from 'typedi';
+import { DatabaseService } from '@app/services/database.service';
+import * as process from 'process';
+import { Quiz } from '@app/interfaces/quiz.interface';
+
+@Service()
+export class ChatroomService {
+    constructor(private readonly dbService: DatabaseService) {}
+
+    get collection() {
+        return this.dbService.database.collection(process.env.DATABASE_COLLECTION_GAMES);
+    }
+
+    async getAll() {
+        return (await this.collection.find({}).toArray()) as unknown[] as Quiz[];
+    }
+
+    async getById(id: string) {
+        const quiz = await this.collection.findOne({ id });
+        return quiz as unknown as Quiz;
+    }
+
+    async add(quiz: Quiz) {
+        await this.collection.insertOne(quiz);
+    }
+
+    async update(quiz: Quiz) {
+        await this.collection.updateOne({ id: quiz.id }, { $set: quiz }, { upsert: true });
+    }
+
+    async delete(id: string) {
+        await this.collection.deleteOne({ id });
+    }
+}
