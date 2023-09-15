fdb613d46ded146a20f8cce1a1c98f21189ce7dd
hb.96.montreal <hb.96.montreal@gmail.com>
2023-09-15 14:28:50 -0400

    Update: game-item component

--- client/src/app/components/game-item/game-item.component.ts
+++ client/src/app/components/game-item/game-item.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Quiz } from '@app/interfaces/quiz'; 
 
 @Component({
-  selector: 'app-game-item',
-  templateUrl: './game-item.component.html',
-  styleUrls: ['./game-item.component.scss']
+    selector: 'app-game-item',
+    templateUrl: './game-item.component.html',
+    styleUrls: ['./game-item.component.scss'],
 })
-export class GameItemComponent {
 
+export class GameItemComponent {
+    @Input() quiz: Quiz;
+
+    deleteGame() {
+        return;
+    }
+
+    updateGame() {
+        return;
+    }
+
+    exportGame() {
+        return;
+    }
 }
