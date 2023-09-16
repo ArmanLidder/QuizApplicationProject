cbab8a669e87b45ebef8ae817e5c72c3e752035d
Rayan Ait Abdesselam <rayan.ait-abdesselam@polymtl.ca>
2023-09-16 01:03:53 -0400

    Refactor: changed name from GameCreation to QuizCreationComponent

--- client/src/app/components/quiz-creation/quiz-creation.component.spec.ts
+++ client/src/app/components/quiz-creation/quiz-creation.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { GameCreation } from '@app/components/quiz-creation/quiz-creation.component';
+import { QuizCreationComponent } from '@app/components/quiz-creation/quiz-creation.component';
 
-describe('GameCreation', () => {
-    let component: GameCreation;
-    let fixture: ComponentFixture<GameCreation>;
+describe('QuizCreation', () => {
+    let component: QuizCreationComponent;
+    let fixture: ComponentFixture<QuizCreationComponent>;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [GameCreation],
+            declarations: [QuizCreationComponent],
         }).compileComponents();
     });
 
     beforeEach(() => {
-        fixture = TestBed.createComponent(GameCreation);
+        fixture = TestBed.createComponent(QuizCreationComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
