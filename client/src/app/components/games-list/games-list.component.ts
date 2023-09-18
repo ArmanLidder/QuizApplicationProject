0105cd735fb705515425644bceb95da11f9c8a29
hb.96.montreal <hb.96.montreal@gmail.com>
2023-09-17 22:24:01 -0400

    feature: ngOnInit + populateGameList

--- client/src/app/components/games-list/games-list.component.ts
+++ client/src/app/components/games-list/games-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { QuizService } from '@app/services/quiz.service';
 import { Quiz } from '@app/interfaces/quiz';
 
 @Component({
@@ -6,169 +7,31 @@ import { Quiz } from '@app/interfaces/quiz';
     templateUrl: './games-list.component.html',
     styleUrls: ['./games-list.component.scss'],
 })
-export class GamesListComponent {
-    @Input()
-    isAdmin: boolean;
-    quizzes: Quiz[] = [
-        {
-            id: '1',
-            title: 'Math Quiz',
-            duration: 30,
-            lastModification: '2023-09-15',
-            questions: [
-                {
-                    type: 0,
-                    text: 'What is 2 + 2?',
-                    points: 5,
-                    choices: [{ text: '3' }, { text: '4', isCorrect: true }, { text: '5' }],
-                },
-                {
-                    type: 1,
-                    text: 'Solve for x: 3x - 7 = 14',
-                    points: 10,
-                },
-            ],
-            visible: true,
-        },
-        {
-            id: '2',
-            title: 'Science Quiz',
-            duration: 45,
-            lastModification: '2023-09-15',
-            questions: [
-                {
-                    type: 0,
-                    text: 'What is the chemical symbol for water?',
-                    points: 5,
-                    choices: [{ text: 'O2' }, { text: 'H2O', isCorrect: true }, { text: 'CO2' }],
-                },
-                {
-                    type: 1,
-                    text: 'What is the boiling point of water in Celsius?',
-                    points: 10,
-                },
-            ],
-            visible: true,
-        },
-        {
-            id: '1',
-            title: 'Math Quiz',
-            duration: 30,
-            lastModification: '2023-09-15',
-            questions: [
-                {
-                    type: 0,
-                    text: 'What is 2 + 2?',
-                    points: 5,
-                    choices: [{ text: '3' }, { text: '4', isCorrect: true }, { text: '5' }],
-                },
-                {
-                    type: 1,
-                    text: 'Solve for x: 3x - 7 = 14',
-                    points: 10,
-                },
-            ],
-            visible: true,
-        },
-        {
-            id: '2',
-            title: 'Science Quiz',
-            duration: 45,
-            lastModification: '2023-09-15',
-            questions: [
-                {
-                    type: 0,
-                    text: 'What is the chemical symbol for water?',
-                    points: 5,
-                    choices: [{ text: 'O2' }, { text: 'H2O', isCorrect: true }, { text: 'CO2' }],
-                },
-                {
-                    type: 1,
-                    text: 'What is the boiling point of water in Celsius?',
-                    points: 10,
-                },
-            ],
-            visible: true,
-        },
-        {
-            id: '1',
-            title: 'Math Quiz',
-            duration: 30,
-            lastModification: '2023-09-15',
-            questions: [
-                {
-                    type: 0,
-                    text: 'What is 2 + 2?',
-                    points: 5,
-                    choices: [{ text: '3' }, { text: '4', isCorrect: true }, { text: '5' }],
-                },
-                {
-                    type: 1,
-                    text: 'Solve for x: 3x - 7 = 14',
-                    points: 10,
-                },
-            ],
-            visible: true,
-        },
-        {
-            id: '2',
-            title: 'Science Quiz',
-            duration: 45,
-            lastModification: '2023-09-15',
-            questions: [
-                {
-                    type: 0,
-                    text: 'What is the chemical symbol for water?',
-                    points: 5,
-                    choices: [{ text: 'O2' }, { text: 'H2O', isCorrect: true }, { text: 'CO2' }],
-                },
-                {
-                    type: 1,
-                    text: 'What is the boiling point of water in Celsius?',
-                    points: 10,
-                },
-            ],
-            visible: false,
-        },
-        {
-            id: '1',
-            title: 'Math Quiz',
-            duration: 30,
-            lastModification: '2023-09-15',
-            questions: [
-                {
-                    type: 0,
-                    text: 'What is 2 + 2?',
-                    points: 5,
-                    choices: [{ text: '3' }, { text: '4', isCorrect: true }, { text: '5' }],
-                },
-                {
-                    type: 1,
-                    text: 'Solve for x: 3x - 7 = 14',
-                    points: 10,
-                },
-            ],
-            visible: true,
-        },
-        {
-            id: '2',
-            title: 'Science Quiz',
-            duration: 45,
-            lastModification: '2023-09-15',
-            questions: [
-                {
-                    type: 0,
-                    text: 'What is the chemical symbol for water?',
-                    points: 5,
-                    choices: [{ text: 'O2' }, { text: 'H2O', isCorrect: true }, { text: 'CO2' }],
-                },
-                {
-                    type: 1,
-                    text: 'What is the boiling point of water in Celsius?',
-                    points: 10,
-                },
-            ],
-            visible: false,
-        },
-    ];
+export class GamesListComponent implements OnInit {
+    @Input() isAdmin: boolean;
+    quizzes: Quiz[];
+    constructor(public quizServices: QuizService) {}
+
+    ngOnInit() {
+        this.populateGameList();
+    }
+
+    populateGameList() {
+        if (this.isAdmin) this.getAllQuizzes();
+        else this.getAllVisibleQuizzes();
+    }
+
+    getAllQuizzes() {
+        this.quizServices.basicGetAll().subscribe((res) => {
+            this.quizzes = res;
+            // console.log(res);
+        });
+    }
+
+    getAllVisibleQuizzes() {
+        this.quizServices.basicGetAllVisible().subscribe((res) => {
+            this.quizzes = res;
+            // console.log(res);
+        });
+    }
 }
