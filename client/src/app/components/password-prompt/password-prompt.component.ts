019e9916570c6420d6cd0c90362a68e17288a7ef
ArmanLidder <arman.lidder@polymtl.ca>
2023-09-18 01:12:23 -0400

    feature: you can enter admin password by hitting the enter key

--- client/src/app/components/password-prompt/password-prompt.component.ts
+++ client/src/app/components/password-prompt/password-prompt.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { AdminAuthenticatorService } from '@app/services/admin-authenticator.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-password-prompt',
@@ -7,15 +8,24 @@ import { AdminAuthenticatorService } from '@app/services/admin-authenticator.ser
     styleUrls: ['./password-prompt.component.scss'],
 })
 export class PasswordPromptComponent {
+    @ViewChild('submitButton') submitButton: ElementRef;
     loginStatus: string | null = '';
     errorMessage: string = 'Invalid password. Please try again!';
     successMessage: string = 'Login Succesful';
     inputBorderColor: string = '';
     textColor: string = '';
-    constructor(public authenticatorService: AdminAuthenticatorService) {}
+    constructor(
+        public authenticatorService: AdminAuthenticatorService,
+        private router: Router,
+    ) {}
 
-    updateLoginStatus() {
+    simulateButtonClick() {
+        this.submitButton.nativeElement.click();
+    }
+
+    updateStatus() {
         this.authenticatorService.validatePassword().subscribe((res) => {
+            this.router.navigate(['/game-admin-page']);
             this.authenticatorService.isValid = res;
             this.loginStatus = this.authenticatorService.isValid ? this.successMessage : this.errorMessage;
             if (this.loginStatus === this.errorMessage) this.showErrorFeedback();
