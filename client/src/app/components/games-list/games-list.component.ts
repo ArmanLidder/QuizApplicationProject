0b1ae1b95c66e23eb6d616495513d0f3d876af53
hb.96.montreal <hb.96.montreal@gmail.com>
2023-09-17 23:31:17 -0400

    feature: add updateVisibility

--- client/src/app/components/games-list/games-list.component.ts
+++ client/src/app/components/games-list/games-list.component.ts
@@ -34,4 +34,25 @@ export class GamesListComponent implements OnInit {
             // console.log(res);
         });
     }
+
+    // Something wrong with tha method (quiz.visible has the right value but request quiz have the inverse)
+    updateVisibility(quiz: Quiz) {
+        console.log(quiz);
+        const newQuiz = {
+            id: quiz.id,
+            duration: quiz.duration,
+            lastModification: quiz.lastModification,
+            questions: quiz.questions,
+            title: quiz.title,
+            visible: !quiz.visible,
+        };
+        this.quizServices.basicPut(newQuiz).subscribe(
+            (res) => {
+                console.log('HTTP Response', res);
+            },
+            (error) => {
+                console.log('HTTP error', error);
+            },
+        );
+    }
 }
