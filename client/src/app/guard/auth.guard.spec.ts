ba5e51f01441c6c1a4b6d1240afcce3f8551364b
ArmanLidder <arman.lidder@polymtl.ca>
2023-09-19 17:23:05 -0400

    test: auth guard test 100% coverage

--- client/src/app/guard/auth.guard.spec.ts
+++ client/src/app/guard/auth.guard.spec.ts
@@ -2,20 +2,33 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AuthGuard } from './auth.guard';
 import { HttpClientModule } from '@angular/common/http';
+import { AdminAuthenticatorService } from '@app/services/admin-authenticator.service';
+import { of } from 'rxjs';
+import SpyObj = jasmine.SpyObj;
 
 describe('AuthGuard', () => {
-    let guard: AuthGuard;
+    let authServiceSpy: SpyObj<AdminAuthenticatorService>;
+    let component: AuthGuard;
 
+    beforeEach(() => {
+        authServiceSpy = jasmine.createSpyObj('authServiceSpy', { validatePassword: of(Boolean) });
+        authServiceSpy.validatePassword.and.returnValue(of(true));
+    });
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [RouterTestingModule, HttpClientModule],
-            providers: [AuthGuard],
-        });
+            providers: [{ provide: AdminAuthenticatorService, useValue: authServiceSpy }],
+        }).compileComponents();
 
-        guard = TestBed.inject(AuthGuard);
+        component = TestBed.inject(AuthGuard);
     });
 
     it('should be created', () => {
-        expect(guard).toBeTruthy();
+        expect(component).toBeTruthy();
+    });
+
+    it('can activate should call authservice validatePassword method', () => {
+        component.canActivate();
+        expect(authServiceSpy.validatePassword).toHaveBeenCalled();
     });
 });
