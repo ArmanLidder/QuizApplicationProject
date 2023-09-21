3a9663dfefe219d94a34900d943f5ec23c0bb6e7
hb.96.montreal <hb.96.montreal@gmail.com>
2023-09-21 13:50:48 -0400

    feature: added new quiz validator

---
+++ client/src/app/services/quiz-validation.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Quiz } from '@app/interfaces/quiz.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuizValidationService {
+
+  isValidQuizFormat(quiz: Quiz): boolean {
+    const isValid = (
+      typeof quiz.id === 'string' &&
+      typeof quiz.title === 'string' &&
+      typeof quiz.description === 'string' &&
+      typeof quiz.duration === 'number' &&
+      typeof quiz.lastModification === 'string' &&
+      this.isValidQuestions(quiz)
+    );
+    return isValid;
+  }
+  
+  private isValidQuestions(quiz: Quiz): boolean {
+    return (
+      Array.isArray(quiz.questions) &&
+      quiz.questions.every((question) =>
+        typeof question.type === 'number' &&
+        typeof question.text === 'string' &&
+        typeof question.points === 'number' &&
+        Array.isArray(question.choices) &&
+            question.choices.every((choice) =>
+              typeof choice.text === 'string' &&
+              (choice.isCorrect === undefined || typeof choice.isCorrect === 'boolean')
+            )
+      ));
+  }
+}
