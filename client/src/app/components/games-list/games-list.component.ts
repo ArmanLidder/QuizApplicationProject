5649b2fcd0dea4b22c3a657ffa7e8a2389786896
hb.96.montreal <hb.96.montreal@gmail.com>
2023-09-21 13:55:10 -0400

    feature: selectFile + uploadFile

--- client/src/app/components/games-list/games-list.component.ts
+++ client/src/app/components/games-list/games-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, ElementRef, NgModule } from '@angular/core';
 import { QuizService } from '@app/services/quiz.service';
+import { QuizValidationService } from '@app/services/quiz-validation.service';
 import { Quiz } from '@app/interfaces/quiz.interface';
 
 @Component({
@@ -8,9 +9,13 @@ import { Quiz } from '@app/interfaces/quiz.interface';
     styleUrls: ['./games-list.component.scss'],
 })
 export class GamesListComponent implements OnInit {
+    @NgModule() importError: string | null;
     @Input() isAdmin: boolean;
+    @Input() isImportError: boolean = false;
+    @ViewChild('fileInput') fileInput: ElementRef<HTMLInputElement>;
     quizzes: Quiz[];
-    constructor(public quizServices: QuizService) {}
+    importedQuiz: Quiz | null;
+    constructor(public quizServices: QuizService, public quizValidator: QuizValidationService ) {}
 
     ngOnInit() {
         this.populateGameList();
@@ -34,7 +39,44 @@ export class GamesListComponent implements OnInit {
     }
 
     updateVisibility(quiz: Quiz) {
-        quiz.visible = !quiz.visible;
-        this.quizServices.basicPatch(quiz.id, quiz.visible).subscribe();
+        this.quizServices.basicPatch(quiz.id, !quiz.visible).subscribe();
+    }
+
+    removeQuiz(id: string) {
+        const index = this.quizzes.findIndex((quiz) => quiz.id === id)
+        this.quizzes.splice(index, 1);
+    }
+
+    selectFile(event: Event) {
+        if (event.target instanceof HTMLInputElement && event.target !== undefined) {
+            const selectedFile = event.target.files && event.target.files[0];
+            if (selectedFile) {
+                if (selectedFile.type === 'application/json') {
+                    const fileReader = new FileReader();
+                    fileReader.onload = (e) => {
+                        try { this.importedQuiz = JSON.parse(e.target?.result as string) as Quiz; }
+                        catch (error) { console.log(error) }
+                    };
+                    fileReader.readAsText(selectedFile)
+                } 
+            }
+        }
+    }
+    
+    uploadFile() {
+        this.fileInput.nativeElement.click();
+        if (this.importedQuiz !== null) {
+            if (this.quizValidator.isValidQuizFormat(this.importedQuiz)) {
+                console.log(this.importedQuiz);
+                    this.quizServices.basicPost(this.importedQuiz).subscribe((res) => {
+                        if(res.status === 201 && this.importedQuiz !== null) {
+                            this.quizzes.push(this.importedQuiz)
+                            this.importedQuiz = null;
+                        }
+                    });
+            }
+        }
     }
 }
+            
+                    
