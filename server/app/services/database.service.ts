ed16527dceeffbb7bd266859172f4f6bdfc1967a
ArmanLidder <arman.lidder@polymtl.ca>
2023-09-28 22:01:38 -0400

    update : moved populate games to a different file

--- server/app/services/database.service.ts
+++ server/app/services/database.service.ts
@@ -1,8 +1,9 @@
 import { Db, MongoClient } from 'mongodb';
 import { Service } from 'typedi';
 import 'dotenv/config';
-import { QuestionType, Quiz } from '@app/interfaces/quiz.interface';
 import * as process from 'process';
+import { fillerQuizzes } from 'assets/data';
+import { Quiz } from '@app/interfaces/quiz.interface';
 
 @Service()
 export class DatabaseService {
@@ -23,56 +24,7 @@ export class DatabaseService {
         }
 
         if ((await this.db.collection(process.env.DATABASE_COLLECTION_QUIZZES).countDocuments()) === 0) {
-            const games = [
-                {
-                    id: '1',
-                    title: 'Math Quiz',
-                    description: 'its a math quiz.',
-                    duration: 30,
-                    lastModification: '2023-09-15',
-                    questions: [
-                        {
-                            type: QuestionType.QCM,
-                            text: 'What is 2 + 2?',
-                            points: 50,
-                            choices: [{ text: '3' }, { text: '4', isCorrect: true }, { text: '5' }],
-                        },
-                    ],
-                    visible: true,
-                },
-                {
-                    id: '2',
-                    title: 'Science Quiz',
-                    description: 'its a science quiz!',
-                    duration: 45,
-                    lastModification: '2023-09-15',
-                    questions: [
-                        {
-                            type: QuestionType.QCM,
-                            text: 'What is the chemical symbol for water?',
-                            points: 50,
-                            choices: [
-                                { text: 'O2', isCorrect: false },
-                                { text: 'H2O', isCorrect: true },
-                                { text: 'CO2', isCorrect: true },
-                            ],
-                        },
-                        {
-                            type: QuestionType.QCM,
-                            text: 'What is the boiling point of water in Celsius?',
-                            points: 10,
-                            choices: [
-                                { text: '0°C', isCorrect: false },
-                                { text: '100°C', isCorrect: true },
-                                { text: '50°C', isCorrect: true },
-                                { text: '-10°C', isCorrect: false },
-                            ],
-                        },
-                    ],
-                    visible: true,
-                },
-            ];
-            await this.populateDB(process.env.DATABASE_COLLECTION_QUIZZES, games);
+            await this.populateDB(process.env.DATABASE_COLLECTION_QUIZZES);
         }
     }
 
@@ -80,8 +32,8 @@ export class DatabaseService {
         return this.client.close();
     }
 
-    async populateDB(collection: string, quizzes: Quiz[]): Promise<void> {
-        // console.log('THIS ADDS DATA TO THE DATABASE, DO NOT USE OTHERWISE');
+    async populateDB(collection: string): Promise<void> {
+        const quizzes : Quiz[] = fillerQuizzes;
         for (const quiz of quizzes) {
             await this.db.collection(collection).insertOne(quiz);
         }
---
+++ server/assets/data.ts
@@ -0,0 +1,51 @@
+import {QuestionType } from "@app/interfaces/quiz.interface";
+
+export const fillerQuizzes = [
+    {
+        id: '1',
+        title: 'Math Quiz',
+        description: 'its a math quiz.',
+        duration: 30,
+        lastModification: '2023-09-15',
+        questions: [
+            {
+                type: QuestionType.QCM,
+                text: 'What is 2 + 2?',
+                points: 50,
+                choices: [{ text: '3' }, { text: '4', isCorrect: true }, { text: '5' }],
+            },
+        ],
+        visible: true,
+    },
+    {
+        id: '2',
+        title: 'Science Quiz',
+        description: 'its a science quiz!',
+        duration: 45,
+        lastModification: '2023-09-15',
+        questions: [
+            {
+                type: QuestionType.QCM,
+                text: 'What is the chemical symbol for water?',
+                points: 50,
+                choices: [
+                    { text: 'O2', isCorrect: false },
+                    { text: 'H2O', isCorrect: true },
+                    { text: 'CO2', isCorrect: true },
+                ],
+            },
+            {
+                type: QuestionType.QCM,
+                text: 'What is the boiling point of water in Celsius?',
+                points: 10,
+                choices: [
+                    { text: '0°C', isCorrect: false },
+                    { text: '100°C', isCorrect: true },
+                    { text: '50°C', isCorrect: true },
+                    { text: '-10°C', isCorrect: false },
+                ],
+            },
+        ],
+        visible: true,
+    },
+];
