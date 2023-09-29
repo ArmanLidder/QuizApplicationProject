9f0005d7ec0f434145bd7c529abca5b9f4f84adc
hb.96.montreal <hb.96.montreal@gmail.com>
2023-09-29 10:21:48 -0400

    Revert "fix : making canActivate only one function without a class"
    This reverts commit 40f8b089b961307543ea786ee1a86b290b0dcc1e.

--- client/src/app/guard/auth.guard.ts
+++ client/src/app/guard/auth.guard.ts
@@ -1,8 +1,19 @@
-import { inject } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 import { AdminAuthenticatorService } from '@app/services/admin-authenticator.service';
 import { Observable } from 'rxjs';
 
+@Injectable({
+    providedIn: 'root',
+})
+class AuthGuard {
+    constructor(public authenticator: AdminAuthenticatorService) {}
+
+    canActivate(): Observable<boolean> {
+        return this.authenticator.validatePassword();
+    }
+}
+
 export const authGuardAuthentification: CanActivateFn = (): Observable<boolean> => {
-    return inject(AdminAuthenticatorService).validatePassword();
+    return inject(AuthGuard).canActivate();
 };
