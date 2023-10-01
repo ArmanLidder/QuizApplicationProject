f83c6104a0cfe88e38b621de1bbadc84e8ff3bf1
ArmanLidder <arman.lidder@polymtl.ca>
2023-10-01 01:39:08 -0400

    fix : NG errors fixed

--- client/src/app/pages/game-creation-page/game-creation-page.component.spec.ts
+++ client/src/app/pages/game-creation-page/game-creation-page.component.spec.ts
@@ -1,24 +1,29 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { SidebarComponent } from '@app/components/sidebar/sidebar.component';
-// import { GameCreationPageComponent } from './game-creation-page.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { SidebarComponent } from '@app/components/sidebar/sidebar.component';
+import { GameCreationPageComponent } from './game-creation-page.component';
+import { GamesListComponent } from '@app/components/games-list/games-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 
-// describe('GameCreationPageComponent', () => {
-//     let component: GameCreationPageComponent;
-//     let fixture: ComponentFixture<GameCreationPageComponent>;
+describe('GameCreationPageComponent', () => {
+    let component: GameCreationPageComponent;
+    let fixture: ComponentFixture<GameCreationPageComponent>;
 
-//     beforeEach(async () => {
-//         await TestBed.configureTestingModule({
-//             declarations: [GameCreationPageComponent, SidebarComponent, GameCreationPageComponent],
-//         }).compileComponents();
-//     });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports : [HttpClientModule, FormsModule, RouterTestingModule],
+            declarations: [GameCreationPageComponent, SidebarComponent, GameCreationPageComponent, GamesListComponent],
+        }).compileComponents();
+    });
 
-//     beforeEach(() => {
-//         fixture = TestBed.createComponent(GameCreationPageComponent);
-//         component = fixture.componentInstance;
-//         fixture.detectChanges();
-//     });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(GameCreationPageComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-//     it('should create', () => {
-//         expect(component).toBeTruthy();
-//     });
-// });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
--- client/src/app/pages/game-page/game-page.component.spec.ts
+++ client/src/app/pages/game-page/game-page.component.spec.ts
@@ -1,25 +1,28 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { PlayAreaComponent } from '@app/components/play-area/play-area.component';
-// import { SidebarComponent } from '@app/components/sidebar/sidebar.component';
-// import { GamePageComponent } from './game-page.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PlayAreaComponent } from '@app/components/play-area/play-area.component';
+import { SidebarComponent } from '@app/components/sidebar/sidebar.component';
+import { GamePageComponent } from './game-page.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from "@angular/router/testing";
+import { FormsModule } from '@angular/forms';
+describe('GamePageComponent', () => {
+    let component: GamePageComponent;
+    let fixture: ComponentFixture<GamePageComponent>;
 
-// describe('GamePageComponent', () => {
-//     let component: GamePageComponent;
-//     let fixture: ComponentFixture<GamePageComponent>;
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports : [HttpClientModule, RouterTestingModule, FormsModule],
+            declarations: [GamePageComponent, SidebarComponent, PlayAreaComponent],
+        }).compileComponents();
+    });
 
-//     beforeEach(async () => {
-//         await TestBed.configureTestingModule({
-//             declarations: [GamePageComponent, SidebarComponent, PlayAreaComponent],
-//         }).compileComponents();
-//     });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(GamePageComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-//     beforeEach(() => {
-//         fixture = TestBed.createComponent(GamePageComponent);
-//         component = fixture.componentInstance;
-//         fixture.detectChanges();
-//     });
-
-//     it('should create', () => {
-//         expect(component).toBeTruthy();
-//     });
-// });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
