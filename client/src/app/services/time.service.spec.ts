fdf7f0b4c2d4c3a82b95e56ed5e17a185f1b1ad5
Adlane <mohamed-adlane.oussaada@polymtl.ca>
2023-10-02 00:51:10 -0400

    Test: Timeservice tests done

--- client/src/app/services/time.service.spec.ts
+++ client/src/app/services/time.service.spec.ts
@@ -1,73 +1,114 @@
-// import { TestBed, discardPeriodicTasks, fakeAsync, tick } from '@angular/core/testing';
+import { TimeService } from './time.service';
+import { TestBed, discardPeriodicTasks, fakeAsync, tick } from '@angular/core/testing';
 
-// import { TimeService } from './time.service';
+describe('TimeService', () => {
+    let component: TimeService;
+    let stopTimerSpy: jasmine.Spy;
+    const DEFAULTTIMERVALUE = 10;
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        component = TestBed.inject(TimeService);
+        component.deleteAllTimers();
+    });
 
-// describe('TimeService', () => {
-//     let service: TimeService;
-//     const TIMEOUT = 5;
-//     const MS_SECOND = 1000;
+    it('should create a timer', () => {
+        const TIMER = component.createTimer(DEFAULTTIMERVALUE);
+        expect(TIMER).toBeDefined();
+        expect(component.timersArray).toContain(TIMER);
+    });
 
-//     beforeEach(() => {
-//         TestBed.configureTestingModule({});
-//         service = TestBed.inject(TimeService);
-//     });
+    it('should delete a timer by index', () => {
+        const TIMER1 = component.createTimer(DEFAULTTIMERVALUE);
+        const TEMPTIMERVALUE = 20;
+        const TIMER2 = component.createTimer(TEMPTIMERVALUE);
 
-//     it('should be created', () => {
-//         expect(service).toBeTruthy();
-//     });
+        component.deleteTimerByIndex(0);
 
-//     it('startTimer should start an interval', fakeAsync(() => {
-//         service.startTimer(TIMEOUT);
-//         const interval = service['interval'];
-//         expect(interval).toBeTruthy();
-//         expect(service.time).toEqual(TIMEOUT);
-//         discardPeriodicTasks();
-//     }));
+        expect(component.timersArray).not.toContain(TIMER1);
+        expect(component.timersArray).toContain(TIMER2);
+    });
 
-//     it('startTimer should call setInterval', fakeAsync(() => {
-//         const spy = spyOn(window, 'setInterval');
-//         service.startTimer(TIMEOUT);
-//         expect(spy).toHaveBeenCalled();
-//         discardPeriodicTasks();
-//     }));
+    it('should delete all timers', () => {
+        const TIMER1 = component.createTimer(DEFAULTTIMERVALUE);
+        const TEMPTIMERVALUE = 20;
+        const TIMER2 = component.createTimer(TEMPTIMERVALUE);
+        stopTimerSpy = spyOn(component, 'stopTimer');
 
-//     it('interval should reduce time by 1 every second ', fakeAsync(() => {
-//         service.startTimer(TIMEOUT);
-//         tick(MS_SECOND);
-//         expect(service.time).toEqual(TIMEOUT - 1);
-//         tick(MS_SECOND);
-//         expect(service.time).toEqual(TIMEOUT - 2);
-//         discardPeriodicTasks();
-//     }));
+        component.deleteAllTimers();
 
-//     it('interval should stop after TIMEOUT seconds ', fakeAsync(() => {
-//         service.startTimer(TIMEOUT);
-//         tick((TIMEOUT + 2) * MS_SECOND);
-//         expect(service.time).toEqual(0);
-//         discardPeriodicTasks();
-//     }));
+        expect(stopTimerSpy).toHaveBeenCalledWith(0);
+        expect(stopTimerSpy).toHaveBeenCalledWith(1);
+        expect(component.timersArray).not.toContain(TIMER1);
+        expect(component.timersArray).not.toContain(TIMER2);
+    });
 
-//     it('startTimer should not start a new interval if one exists', fakeAsync(() => {
-//         service.startTimer(TIMEOUT);
-//         const spy = spyOn(window, 'setInterval');
-//         service.startTimer(TIMEOUT);
-//         expect(spy).not.toHaveBeenCalled();
-//         discardPeriodicTasks();
-//     }));
+    it('should get a timer by index', () => {
+        const TIMER1 = component.createTimer(DEFAULTTIMERVALUE);
 
-//     it('startTimer should call stopTimer at the end of timer', fakeAsync(() => {
-//         const spy = spyOn(service, 'stopTimer').and.callThrough();
-//         service.startTimer(TIMEOUT);
-//         tick((TIMEOUT + 1) * MS_SECOND); // un tick de plus que la limite
-//         expect(spy).toHaveBeenCalled();
-//         discardPeriodicTasks();
-//     }));
+        component.getTime(0);
 
-//     it('stopTimer should call clearInterval and setInterval to undefined', fakeAsync(() => {
-//         const spy = spyOn(window, 'clearInterval');
-//         service.stopTimer();
-//         expect(spy).toHaveBeenCalled();
-//         expect(service['interval']).toBeFalsy();
-//         discardPeriodicTasks();
-//     }));
-// });
+        expect(component.getTimer(0)).toBe(TIMER1);
+    });
+
+    it('should get the initial value of a timer by index', () => {
+        component.createTimer(DEFAULTTIMERVALUE);
+
+        component.getInitialValue(0);
+
+        expect(component.getInitialValue(0)).toBe(DEFAULTTIMERVALUE);
+    });
+
+    it('should set the time of a timer by index', () => {
+        const TEMPTIMERVALUE = 20;
+        component.createTimer(TEMPTIMERVALUE);
+
+        component.setTime(0, DEFAULTTIMERVALUE);
+
+        expect(component.getTime(0)).toBe(DEFAULTTIMERVALUE);
+    });
+
+    it('should not setIntervalValue if intervalValue is already defined', () => {
+        const TEMPINTERVALVALUE = 123;
+        component.createTimer(DEFAULTTIMERVALUE).setIntervalValue(TEMPINTERVALVALUE);
+        component.startTimer(0);
+
+        expect(component.getTimer(0).intervalValue).toBeDefined();
+    });
+
+    it('should setIntervalValue if intervalValue is undefined', () => {
+        component.createTimer(DEFAULTTIMERVALUE).setIntervalValue(undefined);
+        component.startTimer(0);
+
+        expect(component.getTimer(0).intervalValue).toBeDefined();
+    });
+
+    it('should decrement the timer correctly', fakeAsync(() => {
+        const TICKVALUE = 1000;
+        stopTimerSpy = spyOn(component, 'stopTimer');
+        component.createTimer(DEFAULTTIMERVALUE);
+
+        component.startTimer(0);
+        tick(TICKVALUE);
+        expect(component.getTime(0)).toBe(DEFAULTTIMERVALUE - 1);
+        tick(TICKVALUE);
+        expect(component.getTime(0)).toBe(DEFAULTTIMERVALUE - 2);
+        discardPeriodicTasks();
+
+        component.setTime(0, 0);
+        tick(TICKVALUE);
+        expect(stopTimerSpy).toHaveBeenCalledWith(0);
+        discardPeriodicTasks();
+    }));
+
+    it('should stop the timer and set intervalValue to undefined', () => {
+        const TIMER = component.createTimer(DEFAULTTIMERVALUE);
+        component.startTimer(0);
+        const SPYSETINTERVALVALUE = spyOn(TIMER, 'setIntervalValue');
+        spyOn(window, 'clearInterval');
+
+        component.stopTimer(0);
+
+        expect(window.clearInterval).toHaveBeenCalledWith(component.getTimer(0).intervalValue);
+        expect(SPYSETINTERVALVALUE).toHaveBeenCalledWith(undefined);
+    });
+});
