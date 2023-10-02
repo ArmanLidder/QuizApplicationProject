f6dec99be5ec60f424807c3fbd6ec4fea7187d92
Rayan Ait Abdesselam <rayan.ait-abdesselam@polymtl.ca>
2023-10-02 18:39:55 -0400

    feature: added quiz-creation component class and test files

--- client/src/app/services/quiz.service.ts
+++ client/src/app/services/quiz.service.ts
@@ -1,20 +1,24 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { Quiz } from '@app/interfaces/quiz.interface';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root',
 })
 export class QuizService {
-    private readonly baseUrl: string = environment.serverUrl;
+    readonly baseUrl: string = environment.serverUrl;
 
     constructor(private readonly http: HttpClient) {}
 
-    basicGetAll(): Observable<Quiz> {
-        return this.http.get<Quiz>(`${this.baseUrl}/quiz`).pipe(catchError(this.handleError<Quiz>('basicGetAll')));
+    basicGetAll(): Observable<Quiz[]> {
+        return this.http.get<Quiz[]>(`${this.baseUrl}/quiz`).pipe(catchError(this.handleError<Quiz[]>('basicGetAll')));
+    }
+
+    basicGetAllVisible(): Observable<Quiz[]> {
+        return this.http.get<Quiz[]>(`${this.baseUrl}/quiz/visible`).pipe(catchError(this.handleError<Quiz[]>('basicGetAllVisible')));
     }
 
     basicGetById(id: string): Observable<Quiz> {
@@ -22,15 +26,27 @@ export class QuizService {
     }
 
     basicPost(quiz: Quiz): Observable<HttpResponse<string>> {
-        return this.http.post(`${this.baseUrl}/quiz/`, quiz, { observe: 'response', responseType: 'text' });
+        return this.http.post(`${this.baseUrl}/quiz/`, { quiz }, { observe: 'response', responseType: 'text' });
     }
 
     basicPut(quiz: Quiz): Observable<HttpResponse<string>> {
-        return this.http.put(`${this.baseUrl}/quiz/`, quiz, { observe: 'response', responseType: 'text' });
+        return this.http.put(`${this.baseUrl}/quiz/`, { quiz }, { observe: 'response', responseType: 'text' });
     }
 
-    basicDelete(id: number) {
-        this.http.delete(`${this.baseUrl}/quiz/${id}`);
+    basicPatch(quizId: string, quizVisibility: boolean): Observable<HttpResponse<string>> {
+        return this.http.patch(`${this.baseUrl}/quiz/${quizId}`, { visible: quizVisibility }, { observe: 'response', responseType: 'text' });
+    }
+
+    basicDelete(id: string) {
+        return this.http.delete(`${this.baseUrl}/quiz/${id}`);
+    }
+
+    checkTitleUniqueness(title: string): Observable<HttpResponse<{ isUnique: boolean }>> {
+        return this.http.post<{ isUnique: boolean }>(
+            `${this.baseUrl}/quiz/checkTitleUniqueness`,
+            { title },
+            { observe: 'response', responseType: 'json' },
+        );
     }
 
     private handleError<T>(request: string, result?: T): (error: Error) => Observable<T> {
