6c62462793f841628f72c279a39a1be2788e21f8
Rayan Ait Abdesselam <rayan.ait-abdesselam@polymtl.ca>
2023-10-02 18:35:21 -0400

    Feature: added question list component class and test file

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PlayAreaComponent } from '@app/components/play-area/play-area.component';
@@ -11,7 +11,7 @@ import { AppComponent } from '@app/pages/app/app.component';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
-import { QuestionComponent } from './components/question/question.component';
+import { QuestionListComponent } from './components/question/question-list.component';
 import { GameAdministrationPageComponent } from './pages/game-administration-page/game-administration-page.component';
 import { GameCreationPageComponent } from './pages/game-creation-page/game-creation-page.component';
 import { QuizCreationPageComponent } from '@app/pages/quiz-creation-page/quiz-creation-page.component';
@@ -33,11 +33,11 @@ import { QuizCreationComponent } from '@app/components/quiz-creation/quiz-creati
         SidebarComponent,
         QuizCreationComponent,
         QuizCreationPageComponent,
-        QuestionComponent,
+        QuestionListComponent,
         GameCreationPageComponent,
         GameAdministrationPageComponent,
     ],
-    imports: [AppMaterialModule, AppRoutingModule, BrowserAnimationsModule, BrowserModule, FormsModule, HttpClientModule],
+    imports: [AppMaterialModule, AppRoutingModule, BrowserAnimationsModule, BrowserModule, FormsModule, HttpClientModule, ReactiveFormsModule],
     providers: [],
     bootstrap: [AppComponent],
 })
