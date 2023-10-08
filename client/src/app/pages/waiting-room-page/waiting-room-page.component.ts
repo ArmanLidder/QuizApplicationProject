8632f58fc4f20aca7936b3cab688b4750d2560ca
ArmanLidder <arman.lidder@polymtl.ca>
2023-10-08 17:21:41 -0400

    feature:created temporary html for waitingroom

--- client/src/app/pages/waiting-room-page/waiting-room-page.component.ts
+++ client/src/app/pages/waiting-room-page/waiting-room-page.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'app-waiting-room-page',
     templateUrl: './waiting-room-page.component.html',
     styleUrls: ['./waiting-room-page.component.scss'],
 })
-export class WaitingRoomPageComponent {}
+export class WaitingRoomPageComponent {
+    @Input() isHost: boolean;
+    roomCode: number = 1234; //temporary number for viewing
+}
