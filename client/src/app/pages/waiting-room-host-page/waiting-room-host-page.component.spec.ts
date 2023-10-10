d86f06cbf49fe54aacd4345c33eb21670a6ffc38
hb.96.montreal <hb.96.montreal@gmail.com>
2023-10-10 03:33:07 -0400

    feature/added waiting view host

--- client/src/app/pages/waiting-room-host-page/waiting-room-host-page.component.spec.ts
+++ client/src/app/pages/waiting-room-host-page/waiting-room-host-page.component.spec.ts
@@ -3,19 +3,19 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { WaitingRoomHostPageComponent } from './waiting-room-host-page.component';
 
 describe('WaitingRoomHostPageComponent', () => {
-  let component: WaitingRoomHostPageComponent;
-  let fixture: ComponentFixture<WaitingRoomHostPageComponent>;
+    let component: WaitingRoomHostPageComponent;
+    let fixture: ComponentFixture<WaitingRoomHostPageComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [WaitingRoomHostPageComponent]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [WaitingRoomHostPageComponent],
+        });
+        fixture = TestBed.createComponent(WaitingRoomHostPageComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(WaitingRoomHostPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- client/src/app/pages/waiting-room-host-page/waiting-room-host-page.component.ts
+++ client/src/app/pages/waiting-room-host-page/waiting-room-host-page.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-waiting-room-host-page',
-  templateUrl: './waiting-room-host-page.component.html',
-  styleUrls: ['./waiting-room-host-page.component.scss']
+    selector: 'app-waiting-room-host-page',
+    templateUrl: './waiting-room-host-page.component.html',
+    styleUrls: ['./waiting-room-host-page.component.scss'],
 })
-export class WaitingRoomHostPageComponent {
-
-}
+export class WaitingRoomHostPageComponent {}
