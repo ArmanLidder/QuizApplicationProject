fea8db39080aecf3b57b5e67f09f1f066456ab71
ArmanLidder <arman.lidder@polymtl.ca>
2023-10-09 23:46:24 -0400

    refactor : moved code to a component

--- client/src/app/components/waiting-room/waiting-room.component.ts
+++ client/src/app/components/waiting-room/waiting-room.component.ts
@@ -1,10 +1,98 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { SocketClientService } from '@app/services/socket-client.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-waiting-room',
   templateUrl: './waiting-room.component.html',
   styleUrls: ['./waiting-room.component.scss']
 })
-export class WaitingRoomComponent {
+export class WaitingRoomComponent implements OnInit{
+  @Input() isHost: boolean = true; // temporary set to true for testing functionality
+  roomId: number;
+  players: string[] = ['karim', 'benzema'];
+  readonly DELETE_NUMBER = 1;
 
+  constructor(
+      public socketService: SocketClientService,
+      private readonly route: ActivatedRoute,
+      private router: Router
+  ) {}
+
+  get socketId() {
+    return this.socketService.socket.id ? this.socketService.socket.id : '';
+  }
+
+  ngOnInit() {
+    this.connect();
+    if (this.isHost)
+      this.sendRoomCreation();
+  }
+
+  connect() {
+    if (!this.socketService.isSocketAlive()) {
+      this.socketService.connect();
+      this.configureBaseSocketFeatures();
+    }
+  }
+
+  banPlayer(username: string) {
+    this.sendBanPlayer(username)
+  }
+
+  toggleRoomLocked() {
+    this.sendToggleRoomLock()
+  }
+
+  startGame() {
+    this.sendStartSignal();
+  }
+
+  private sendRoomCreation() {
+    const QUIZ_ID = this.route.snapshot.paramMap.get('id');
+    this.socketService.send('create Room', QUIZ_ID, (roomCode: number) => {
+      this.roomId = roomCode;
+    });
+  }
+
+  private configureBaseSocketFeatures() {
+    this.socketService.on('connect', () => {
+      console.log(`Connexion par WebSocket sur le socket ${this.socketId}`);
+    });
+
+    this.socketService.on('new player', (username: string) => {
+      this.addPlayer(username);
+    });
+
+    this.socketService.on('you have been banned',() => {
+      this.router.navigate(['/home']);
+    });
+
+    this.socketService.on('banned player', (username: string) => {
+      this.removePlayer(username)
+    })
+  }
+
+  private sendBanPlayer( username: string) {
+    this.socketService.send('ban player', {roomId: this.roomId, username: username});
+  }
+
+  private sendToggleRoomLock() {
+    this.socketService.send('toggle room lock', this.roomId);
+  }
+
+  private sendStartSignal() {
+    this.socketService.send('start', this.roomId);
+  }
+
+  private addPlayer(username: string) {
+    this.players.push(username);
+  }
+
+  private removePlayer(username: string) {
+    const index = this.players.indexOf(username);
+    this.players.splice(index, this.DELETE_NUMBER);
+  }
 }
+
+
--- client/src/app/pages/waiting-room-page/waiting-room-page.component.ts
+++ client/src/app/pages/waiting-room-page/waiting-room-page.component.ts
@@ -1,96 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { SocketClientService } from '@app/services/socket-client.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-waiting-room-page',
     templateUrl: './waiting-room-page.component.html',
     styleUrls: ['./waiting-room-page.component.scss'],
 })
-export class WaitingRoomPageComponent implements OnInit {
-    @Input() isHost: boolean = true; // temporary set to true for testing functionality
-    roomId: number;
-    players: string[] = ['karim', 'benzema'];
-    readonly DELETE_NUMBER = 1;
+export class WaitingRoomPageComponent {
 
-    constructor(
-        public socketService: SocketClientService,
-        private readonly route: ActivatedRoute,
-        private router: Router
-    ) {}
-
-    get socketId() {
-        return this.socketService.socket.id ? this.socketService.socket.id : '';
-    }
-
-    ngOnInit() {
-        this.connect();
-        if (this.isHost)
-            this.sendRoomCreation();
-    }
-
-    connect() {
-        if (!this.socketService.isSocketAlive()) {
-            this.socketService.connect();
-            this.configureBaseSocketFeatures();
-        }
-    }
-
-    banPlayer(username: string) {
-            this.sendBanPlayer(username)
-    }
-
-    toggleRoomLocked() {
-        this.sendToggleRoomLock()
-    }
-
-    startGame() {
-        this.sendStartSignal();
-    }
-
-    private sendRoomCreation() {
-        const QUIZ_ID = this.route.snapshot.paramMap.get('id');
-        this.socketService.send('create Room', QUIZ_ID, (roomCode: number) => {
-            this.roomId = roomCode;
-        });
-    }
-
-    private configureBaseSocketFeatures() {
-        this.socketService.on('connect', () => {
-            console.log(`Connexion par WebSocket sur le socket ${this.socketId}`);
-        });
-
-        this.socketService.on('new player', (username: string) => {
-           this.addPlayer(username);
-        });
-
-        this.socketService.on('you have been banned',() => {
-            this.router.navigate(['/home']);
-        });
-
-        this.socketService.on('banned player', (username: string) => {
-            this.removePlayer(username)
-        })
-    }
-
-    private sendBanPlayer( username: string) {
-        this.socketService.send('ban player', {roomId: this.roomId, username: username});
-    }
-
-    private sendToggleRoomLock() {
-        this.socketService.send('toggle room lock', this.roomId);
-    }
-
-    private sendStartSignal() {
-        this.socketService.send('start', this.roomId);
-    }
-
-    private addPlayer(username: string) {
-        this.players.push(username);
-    }
-
-    private removePlayer(username: string) {
-        const index = this.players.indexOf(username);
-        this.players.splice(index, this.DELETE_NUMBER);
-    }
 }
