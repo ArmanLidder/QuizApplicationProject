99ce8b135db4efbb97cb36e131aa1e1060ddf843
hb.96.montreal <hb.96.montreal@gmail.com>
2023-10-10 03:34:59 -0400

    upgrade/added server socket config

--- server/app/services/socket-manager.service.ts
+++ server/app/services/socket-manager.service.ts
@@ -18,10 +18,13 @@ export class SocketManager {
             // message initial
             socket.emit('hello', 'Hello World!');
 
-            // TODO create service to configure those event reception for organizer view
+            // TODO create service to configure those event reception for organizer waiting view
             socket.on('create Room', (quizID: string, callback) => {
+
                 const roomCode = this.roomManager.addRoom(quizID);
+                console.log(this.roomManager.roomMap.has(roomCode));
                 socket.join(String(roomCode))
+                console.log(this.roomManager.roomMap);
                 callback(roomCode);
             });
 
@@ -32,15 +35,38 @@ export class SocketManager {
                 this.sio.to(bannedID).emit('you have been banned');
             });
 
-            socket.on('player join', (data: { roomId: number; username: string })=> {
-                this.roomManager.addUser(data.roomId,data.username,socket.id);
-                socket.join(String(data.roomId));
-            });
-
+            // Todo verify if it works
             socket.on('toggle room lock', (roomId : number) => {
                 this.roomManager.changeLockState(roomId);
             });
             // For above create service to configure those event reception for organizer view
+
+            // TODO create service to configure those event reception for player joining view
+            socket.on('player join', (data: { roomId: number; username: string }, callback)=> {
+                this.roomManager.addUser(data.roomId,data.username,socket.id);
+                const room = this.roomManager.roomMap.get(data.roomId);
+                const players = Array.from(room.players.keys())
+                socket.join(String(data.roomId));
+                this.sio.to(String(data.roomId)).emit('new player', players);
+            });
+
+            socket.on('validate username', (data: { roomId: number; username: string }, callback) => {
+                if (this.roomManager.isNameUsed(data.roomId, data.username)) {
+                    callback({isValid: false, error : 'Le nom choisi est déjà utiliser. Veuillez choisir un autre.'});
+               } else if (this.roomManager.isNameBanned(data.roomId, data.username)) {
+                   callback({isValid: false, error : 'Vous avez été banni du lobby et vous ne pouvez plus rentrez.'});
+               } else {
+                   callback({isValid: true, error : ''});
+               }
+                // Todo complete by checking also in the banned list and upgrade (still case sensitive)
+            })
+
+            socket.on('validate roomID', ( roomId: number, callback) => {
+                callback(this.roomManager.roomMap.has(roomId));
+            })
+
+            // For above create service to configure those event reception for organizer view
+
 
 
             socket.on('disconnect', (reason) => {
