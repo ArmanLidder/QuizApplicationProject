9922645f54dc6e4c1e7dabb33e92c2cf62d210d4
hb.96.montreal <hb.96.montreal@gmail.com>
2023-10-10 03:33:47 -0400

    feature/added waiting view player

--- client/src/app/pages/waiting-room-player-page/waiting-room-player-page.component.ts
+++ client/src/app/pages/waiting-room-player-page/waiting-room-player-page.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-waiting-room-player-page',
-  templateUrl: './waiting-room-player-page.component.html',
-  styleUrls: ['./waiting-room-player-page.component.scss']
+    selector: 'app-waiting-room-player-page',
+    templateUrl: './waiting-room-player-page.component.html',
+    styleUrls: ['./waiting-room-player-page.component.scss'],
 })
-export class WaitingRoomPlayerPageComponent {
-
-}
+export class WaitingRoomPlayerPageComponent {}
