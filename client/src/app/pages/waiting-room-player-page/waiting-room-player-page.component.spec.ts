ef87c0493599550968ebe254f8b074fb46527357
hb.96.montreal <hb.96.montreal@gmail.com>
2023-10-12 18:22:09 -0400

    fix/solved NG0303 for unit test
    ignoring child component since we are unit testing and not integration testin

--- client/src/app/pages/waiting-room-player-page/waiting-room-player-page.component.spec.ts
+++ client/src/app/pages/waiting-room-player-page/waiting-room-player-page.component.spec.ts
@@ -1,22 +1,22 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { WaitingRoomPlayerPageComponent } from './waiting-room-player-page.component';
-// import { WaitingRoomComponent } from '@app/components/waiting-room/waiting-room.component';
-// import { RoomCodePromptComponent } from '@app/components/room-code-prompt/room-code-prompt.component';
-//
-// describe('WaitingRoomPlayerPageComponent', () => {
-//     let component: WaitingRoomPlayerPageComponent;
-//     let fixture: ComponentFixture<WaitingRoomPlayerPageComponent>;
-//
-//     beforeEach(() => {
-//         TestBed.configureTestingModule({
-//             declarations: [WaitingRoomPlayerPageComponent, WaitingRoomComponent, RoomCodePromptComponent],
-//         });
-//         fixture = TestBed.createComponent(WaitingRoomPlayerPageComponent);
-//         component = fixture.componentInstance;
-//         fixture.detectChanges();
-//     });
-//
-//     it('should create', () => {
-//         expect(component).toBeTruthy();
-//     });
-// });
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { WaitingRoomPlayerPageComponent } from './waiting-room-player-page.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+
+describe('WaitingRoomPlayerPageComponent', () => {
+    let component: WaitingRoomPlayerPageComponent;
+    let fixture: ComponentFixture<WaitingRoomPlayerPageComponent>;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [WaitingRoomPlayerPageComponent],
+            schemas: [NO_ERRORS_SCHEMA],
+        });
+        fixture = TestBed.createComponent(WaitingRoomPlayerPageComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
