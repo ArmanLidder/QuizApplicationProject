119ea7d69b25610fc4f245fd0c25f83759b2f6fb
hb.96.montreal <hb.96.montreal@gmail.com>
2023-10-11 22:54:33 -0400

    remove/Comment

--- client/src/app/services/socket-client.service.ts
+++ client/src/app/services/socket-client.service.ts
@@ -6,7 +6,6 @@ import { io, Socket } from 'socket.io-client';
 })
 export class SocketClientService {
     socket: Socket;
-    // type CallbackFunction = () => void;
 
     isSocketAlive() {
         return this.socket && this.socket.connected;
