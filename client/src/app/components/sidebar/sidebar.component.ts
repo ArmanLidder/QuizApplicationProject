f2017b23f3b065d79b4ae56aac46f7fa0f932f9b
Rayan Ait Abdesselam <rayan.ait-abdesselam@polymtl.ca>
2023-10-12 02:10:20 -0400

    feature: able to get room messages when component constructs

--- client/src/app/components/sidebar/sidebar.component.ts
+++ client/src/app/components/sidebar/sidebar.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Message } from '@common/interfaces/message.interface';
+import { SocketClientService } from '@app/services/socket-client.service';
 
 @Component({
     selector: 'app-sidebar',
     templateUrl: './sidebar.component.html',
     styleUrls: ['./sidebar.component.scss'],
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+    @Input() roomId: number;
+    @Input() myName: string;
+    messages: Message[];
+
+    constructor(public socketService: SocketClientService) {
+        if (this.roomId) {
+            this.getRoomMessages();
+        }
+    }
+
+    private getRoomMessages() {
+        this.socketService.send('player join', { roomId: Number(this.roomId) }, (messages: Message[]) => {
+            this.messages = messages;
+        });
+    }
+}
