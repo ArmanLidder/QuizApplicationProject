aac99933ecabb2b342f6d6d65952fde1b4da241f
hb.96.montreal <hb.96.montreal@gmail.com>
2023-10-12 03:32:05 -0400

    test/initial commit

--- client/src/app/components/waiting-room/waiting-room.component.spec.ts
+++ client/src/app/components/waiting-room/waiting-room.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { WaitingRoomComponent } from './waiting-room.component';
+import { SocketClientService } from '@app/services/socket-client.service';
+import { ActivatedRoute } from '@angular/router';
 
 describe('WaitingRoomComponent', () => {
     let component: WaitingRoomComponent;
@@ -9,6 +10,15 @@ describe('WaitingRoomComponent', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
             declarations: [WaitingRoomComponent],
+            providers: [
+                SocketClientService,
+                {
+                    provide: ActivatedRoute,
+                    useValue: () => {
+                        return '1';
+                    },
+                },
+            ],
         });
         fixture = TestBed.createComponent(WaitingRoomComponent);
         component = fixture.componentInstance;
