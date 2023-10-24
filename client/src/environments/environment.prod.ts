2a444b2f58db5911c7e42369d7a1d648cfc4cd6f
ArmanLidder <arman.lidder@polymtl.ca>
2023-10-24 14:59:43 -0400

    fix: accidently stopped instance, had to change ip

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    serverUrl: 'http://3.97.16.125:3000/api', // TODO: Add remote server URL
+    serverUrl: 'http://99.79.36.151:3000/api', // TODO: Add remote server URL
 };
