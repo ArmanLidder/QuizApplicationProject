081cb8079b807a58d947553e8ee0427368f028df
ArmanLidder <arman.lidder@polymtl.ca>
2023-10-24 02:12:43 -0400

    deploy: deployment url added

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    serverUrl: 'myServerURL', // TODO: Add remote server URL
+    serverUrl: '3.97.16.125:3000', // TODO: Add remote server URL
 };
