1d3084a1df1f5acf31575402c0b3b2ce95f97a5a
Rayan Ait Abdesselam <rayan.ait-abdesselam@polymtl.ca>
2023-10-24 22:02:39 -0400

    refactor: fix lint errors

--- server/app/classes/game.ts
+++ server/app/classes/game.ts
@@ -1,16 +1,16 @@
-import {Quiz, QuizQuestion, QuizChoice} from '@common/interfaces/quiz.interface';
+import { Quiz, QuizQuestion, QuizChoice } from '@common/interfaces/quiz.interface';
 import { QuizService } from '@app/services/quiz.service';
-import { Answers, Score} from '@app/interface/game-interface';
+import { Answers, Score } from '@app/interface/game-interface';
 
 type Username = string;
 type Players = Map<Username, Score>;
 type PlayerAnswers = Map<Username, Answers>;
-type ChoiceStats = Map<string, number>
+type ChoiceStats = Map<string, number>;
 
 const BONUS_MULTIPLIER = 1.2;
 
 export class Game {
-    curr_index: number = 0;
+    currIndex: number = 0;
     quiz: Quiz;
     players: Players = new Map();
     currentQuizQuestion: QuizQuestion;
@@ -20,31 +20,35 @@ export class Game {
     duration: number;
     playersAnswers: PlayerAnswers = new Map();
 
-    constructor(usernames: string[], quizId: string, private readonly quizService: QuizService) {
+    constructor(
+        usernames: string[],
+        quizId: string,
+        private readonly quizService: QuizService,
+    ) {
         this.configurePlayers(usernames);
         this.getQuiz(quizId);
     }
 
     next() {
-        this.updateScores()
+        this.updateScores();
         this.playersAnswers.clear();
-        this.curr_index++;
+        this.currIndex++;
         this.setValues();
     }
 
-    storePlayerAnswer(username: string, time: number, playerAnswer: string[]){
+    storePlayerAnswer(username: string, time: number, playerAnswer: string[]) {
         this.updateChoicesStats(playerAnswer);
-        this.playersAnswers.set(username, {answers: playerAnswer, time: time});
+        this.playersAnswers.set(username, { answers: playerAnswer, time });
     }
 
-    removePlayer(username: string){
+    removePlayer(username: string) {
         this.playersAnswers.delete(username);
     }
 
     private updateChoicesStats(playerAnswer: string[]) {
         playerAnswer.forEach((answer: string) => {
-            if(this.choicesStats.has(answer)) {
-                let oldValue = this.choicesStats.get(answer);
+            if (this.choicesStats.has(answer)) {
+                const oldValue = this.choicesStats.get(answer);
                 this.choicesStats.set(answer, oldValue + 1);
             }
         });
@@ -52,37 +56,37 @@ export class Game {
 
     private updateScores() {
         this.playersAnswers.forEach((player, username) => {
-            if(this.validateAnswer(player.answers)) this.handleGoodAnswer(username);
+            if (this.validateAnswer(player.answers)) this.handleGoodAnswer(username);
             else this.handleWrongAnswer(username);
         });
     }
 
     private validateAnswer(playerAnswers: string[]) {
-        for (let answer of playerAnswers) {
+        for (const answer of playerAnswers) {
             if (!this.correctChoices.includes(answer)) return false;
         }
         return true;
     }
 
     private handleGoodAnswer(username: string) {
-        const oldScore =  this.players.get(username);
+        const oldScore = this.players.get(username);
         const points = this.currentQuizQuestion.points;
         let newScore: Score;
         if (this.getFastestPlayer()) {
             newScore = {
                 points: oldScore.points + this.addBonusPoint(points),
                 bonusCount: oldScore.bonusCount + 1,
-            }
+            };
         } else {
             newScore = {
                 points: oldScore.points + points,
                 bonusCount: oldScore.bonusCount,
-            }
+            };
         }
         this.players.set(username, newScore);
     }
 
-    private addBonusPoint(points: number){
+    private addBonusPoint(points: number) {
         return points * BONUS_MULTIPLIER;
     }
 
@@ -91,20 +95,20 @@ export class Game {
     }
 
     private getAllPlayersCorrectAnswer() {
-        let playersCorrectAnswer: PlayerAnswers= new Map();
+        const playersCorrectAnswer: PlayerAnswers = new Map();
         this.playersAnswers.forEach((player, username) => {
-            if(this.validateAnswer(player.answers)){
+            if (this.validateAnswer(player.answers)) {
                 playersCorrectAnswer.set(username, player);
             }
-        })
+        });
         return playersCorrectAnswer;
     }
 
     private getFastestPlayer() {
         let lowestTime = Infinity;
-        let lowestTimePlayers: PlayerAnswers = new Map();
+        const lowestTimePlayers: PlayerAnswers = new Map();
         const playerAnswers = this.getAllPlayersCorrectAnswer();
-        for (let [username, answers] of playerAnswers) {
+        for (const [username, answers] of playerAnswers) {
             if (answers.time < lowestTime) {
                 lowestTime = answers.time;
                 lowestTimePlayers.clear();
@@ -113,17 +117,17 @@ export class Game {
                 lowestTimePlayers.set(username, answers);
             }
         }
-        return (lowestTimePlayers.size === 1) ? lowestTimePlayers : null;
+        return lowestTimePlayers.size === 1 ? lowestTimePlayers : null;
     }
 
     private configurePlayers(usernames: string[]) {
         usernames.forEach((username) => {
             this.players.set(username, { points: 0, bonusCount: 0 });
-        })
+        });
     }
 
-    private setValues(){
-        this.currentQuizQuestion = this.quiz.questions[this.curr_index];
+    private setValues() {
+        this.currentQuizQuestion = this.quiz.questions[this.currIndex];
         this.question = this.currentQuizQuestion.text;
         this.getAllCorrectChoices();
         this.duration = this.quiz.duration;
@@ -132,14 +136,14 @@ export class Game {
         });
     }
 
-    private getAllCorrectChoices(){
+    private getAllCorrectChoices() {
         this.currentQuizQuestion.choices.forEach((choice: QuizChoice) => {
-            if(choice.isCorrect) this.correctChoices.push(choice.text)
+            if (choice.isCorrect) this.correctChoices.push(choice.text);
         });
     }
 
     private getQuiz(quizId: string) {
-        this.quizService.getById(quizId).then( (quiz: Quiz) => {
+        this.quizService.getById(quizId).then((quiz: Quiz) => {
             this.quiz = quiz;
             this.setValues();
         });
