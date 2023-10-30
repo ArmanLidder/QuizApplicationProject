cc9242ea8218939e2d984979dea9d27714a0422e
hb.96.montreal <hb.96.montreal@gmail.com>
2023-10-30 19:58:32 -0400

    feature/combine host and game interface

--- client/src/app/pages/game-page/game-page.component.ts
+++ client/src/app/pages/game-page/game-page.component.ts
@@ -1,7 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, ViewChild } from '@angular/core';
+import { GameService } from '@app/services/game.service';
+import { SocketClientService } from '@app/services/socket-client.service';
+// import { Router } from '@angular/router';
+import { HostInterfaceComponent } from '@app/components/host-interface/host-interface.component';
+import { GameInterfaceComponent } from '@app/components/game-interface/game-interface.component';
+
 @Component({
     selector: 'app-game-page',
     templateUrl: './game-page.component.html',
     styleUrls: ['./game-page.component.scss'],
 })
-export class GamePageComponent {}
+export class GamePageComponent implements OnDestroy {
+    @ViewChild('app-game-interface') hostComponent: HostInterfaceComponent;
+    @ViewChild('app-host-interface') playerComponent: GameInterfaceComponent;
+    constructor(
+        private gameService: GameService,
+        private readonly socketService: SocketClientService,
+    ) {}
+
+    get isHost(): boolean {
+        return this.gameService.username === 'Organisateur';
+    }
+
+    ngOnDestroy(){
+        const messageType = this.isHost ? 'host abandonment' : 'player abandonment';
+        this.socketService.send(messageType, this.gameService.roomId);
+        this.gameService.destroy();
+        console.log("Game page Destroy")
+    }
+}
