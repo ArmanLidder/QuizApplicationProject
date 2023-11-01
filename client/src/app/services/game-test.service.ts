79f6be73b6d8140b8d37ccf5776c0a6818e2650f
Adlane <mohamed-adlane.oussaada@polymtl.ca>
2023-10-31 23:24:04 -0400

    feature: game-test service update

--- client/src/app/services/game-test.service.ts
+++ client/src/app/services/game-test.service.ts
@@ -1,23 +1,36 @@
 import { Injectable } from '@angular/core';
-import { QuizQuestion } from '@common/interfaces/quiz.interface';
+import { Quiz, QuizQuestion } from '@common/interfaces/quiz.interface';
 import { QuizService } from '@app/services/quiz.service';
 
 @Injectable({
     providedIn: 'root',
 })
 export class GameTestService {
-    username: string = '';
-    roomId: number = 0;
+    quiz: Quiz;
     timer: number = 0;
     question: QuizQuestion | null = null;
     isLast: boolean = false;
     locked: boolean = false;
     validated: boolean = false;
     answers: Map<number, string | null> = new Map();
-    currQuestionIndex: number = 1;
+    currQuestionIndex: number = 0;
 
-    constructor(private quizService: QuizService) {
-        // TODO
+    constructor(private quizService: QuizService) {}
+
+    getQuiz(quizId: string) {
+        this.quizService.basicGetById(quizId).subscribe((quiz: Quiz) => {
+            this.quiz = quiz;
+            this.question = this.quiz.questions[this.currQuestionIndex];
+        })
+    }
+
+    startQuestionTimer() {
+        //
+    }
+
+    next() {
+        this.currQuestionIndex++;
+        this.question = this.quiz.questions[this.currQuestionIndex];
     }
 
     updateScore() {
