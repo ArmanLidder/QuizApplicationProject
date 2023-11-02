e6944a840d342b1e52dee914509c6e5bf3c8511f
Rayan Ait Abdesselam <rayan.ait-abdesselam@polymtl.ca>
2023-11-02 16:51:29 -0400

    feature: changed gameService to redirect to either GameRealService or GameTestService

--- client/src/app/services/game.service.ts
+++ client/src/app/services/game.service.ts
@@ -1,178 +1,95 @@
 import { Injectable } from '@angular/core';
-import { QuizQuestion } from '@common/interfaces/quiz.interface';
-import { SocketClientService } from '@app/services/socket-client.service';
 import { GameTestService } from '@app/services/game-test.service';
-
-type Score = Map<string, number>;
-const TESTING_TRANSITION_TIMER = 3;
+import { GameRealService } from '@app/services/game-real-service';
 
 @Injectable({
     providedIn: 'root',
 })
 export class GameService {
-    timeouts: number[] = [0, 0];
-    quizId: string;
-
+    isTestMode: boolean = false;
     isInputFocused: boolean = false;
-    username: string = '';
-    roomId: number = 0;
-    timer: number = 0;
-    question: QuizQuestion | null = null;
-    isLast: boolean = false;
-    locked: boolean = false;
-    validated: boolean = false;
-    players: Map<string, Score> = new Map();
     answers: Map<number, string | null> = new Map();
-    questionNumber: number = 1;
 
     constructor(
-        public socketService: SocketClientService,
         public gameTestService: GameTestService,
-    ) {
-        if (this.socketService.isSocketAlive()) {
-            this.configureBaseSockets();
-        }
+        public gameRealService: GameRealService,
+    ) {}
+
+    get timer() {
+        return this.isTestMode ? this.gameTestService.timer?.time : this.gameRealService.timer;
     }
 
-    get testTimer() {
-        return this.gameTestService.timer?.time;
-    }
-
-    get testPlayerScore() {
+    get playerScore() {
         return this.gameTestService.playerScore;
     }
 
-    get testIsBonus() {
+    get isBonus() {
         return this.gameTestService.isBonus;
+    }
+
+    get gameOver() {
+        return this.gameTestService.gameOver;
+    }
+
+    get question() {
+        return this.isTestMode ? this.gameTestService.question : this.gameRealService.question;
+    }
+
+    get questionNumber() {
+        return this.isTestMode ? this.gameTestService.currQuestionIndex + 1 : this.gameRealService.questionNumber;
+    }
+
+    get username() {
+        return this.gameRealService.username;
+    }
+
+    get lockedStatus() {
+        return this.isTestMode ? this.gameTestService.locked : this.gameRealService.locked;
+    }
+
+    get validatedStatus() {
+        return this.isTestMode ? this.gameTestService.validated : this.gameRealService.validated;
     }
 
     destroy() {
         this.reset();
-        if (this.socketService.isSocketAlive()) this.socketService.socket.offAny();
     }
 
-    init() {
-        if (this.socketService.isSocketAlive()) {
-            this.configureBaseSockets();
-            this.socketService.send('get question', this.roomId);
+    init(pathId: string) {
+        if (!this.isTestMode) {
+            this.gameRealService.roomId = Number(pathId);
+            this.gameRealService.init();
         } else {
-            this.gameTestService.getQuiz(this.quizId).subscribe((quiz) => {
-                this.gameTestService.quiz = quiz;
-                this.gameTestService.question = quiz.questions[this.gameTestService.currQuestionIndex];
-                this.question = this.gameTestService.question;
-                this.gameTestService.timeService.deleteAllTimers();
-                this.gameTestService.startTimer(this.gameTestService.quiz.duration);
-                this.handleQuestionTimer();
-            });
+            this.gameTestService.quizId = pathId;
+            this.gameTestService.init();
         }
     }
 
     selectChoice(index: number) {
-        if (!this.locked) {
+        if (!this.lockedStatus) {
             if (this.answers.has(index)) {
                 this.answers.delete(index);
             } else {
                 const textChoice = this.question?.choices ? this.question.choices[index].text : null;
                 this.answers.set(index, textChoice);
             }
+            // send to update chart
         }
     }
 
     sendAnswer() {
-        const answers = Array.from(this.answers.values());
-        if (this.socketService.isSocketAlive()) {
-            this.socketService.send('submit answer', {
-                roomId: this.roomId,
-                answers,
-                timer: this.timer,
-                username: this.username,
-            });
+        if (!this.isTestMode) {
+            this.gameRealService.answers = this.answers;
+            this.gameRealService.sendAnswer();
         } else {
-            this.validated = true;
-            this.locked = true;
-            clearTimeout(this.timeouts[0]);
-            this.gameTestService.updateScore(this.answers);
-            this.gameTestService.startTimer(TESTING_TRANSITION_TIMER);
-            this.handleTransitionTimer();
+            this.gameTestService.answers = this.answers;
+            this.gameTestService.sendAnswer();
         }
-        this.locked = true;
         this.answers.clear();
-    }
-
-    configureBaseSockets() {
-        this.socketService.on('get initial question', (data: { question: QuizQuestion; username: string; index: number }) => {
-            this.question = data.question;
-            this.username = data.username;
-        });
-
-        this.socketService.on('get next question', (data: { question: QuizQuestion; index: number; isLast: boolean }) => {
-            this.question = data.question;
-            this.questionNumber = data.index;
-            this.isLast = data.isLast;
-            this.validated = false;
-            this.locked = false;
-        });
-
-        this.socketService.on('time', (timeValue: number) => {
-            this.handleTimeEvent(timeValue);
-        });
-    }
-
-    private handleQuestionTimer() {
-        const tick = 1000;
-        this.timeouts[0] = window.setTimeout(() => {
-            this.sendAnswer();
-        }, this.gameTestService.quiz.duration * tick);
-    }
-
-    private handleTransitionTimer() {
-        const tick = 1000;
-        this.timeouts[1] = window.setTimeout(() => {
-            this.hideFeedback();
-            if (this.gameTestService.next()) {
-                this.questionNumber++;
-                this.question = this.gameTestService.question;
-                this.gameTestService.startTimer(this.gameTestService.quiz.duration);
-                this.handleQuestionTimer();
-            } else {
-                this.showFeedBack();
-            }
-        }, TESTING_TRANSITION_TIMER * tick);
-    }
-
-    private hideFeedback() {
-        this.validated = false;
-        this.locked = false;
-        this.gameTestService.isBonus = false;
-    }
-
-    private showFeedBack() {
-        this.isLast = true;
-        this.validated = true;
-        this.locked = true;
-    }
-
-    private handleTimeEvent(timeValue: number) {
-        this.timer = timeValue;
-        if (this.timer === 0 && !this.locked) {
-            this.locked = true;
-            if (this.username !== 'Organisateur') this.sendAnswer();
-        }
     }
 
     private reset() {
-        this.username = '';
-        this.roomId = 0;
-        this.timer = 0;
-        this.question = null;
-        this.locked = false;
-        this.validated = false;
-        this.isLast = false;
-        this.players.clear();
-        this.answers.clear();
-        this.questionNumber = 1;
-        clearTimeout(this.timeouts[0]);
-        clearTimeout(this.timeouts[1]);
+        this.gameRealService.destroy();
         this.gameTestService.reset();
     }
 }
