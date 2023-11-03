9ba20c5e6a93d47843d8a1e57a5bc85a26248070
ArmanLidder <arman.lidder@polymtl.ca>
2023-11-02 23:19:14 -0400

    feature : histogram component completed

--- client/src/app/components/organizer-histogram/organizer-histogram.component.spec.ts
+++ client/src/app/components/organizer-histogram/organizer-histogram.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { OrganizerHistogramComponent } from './organizer-histogram.component';
+import { NgChartsModule } from 'ng2-charts';
 
 describe('OrganizerHistogramComponent', () => {
   let component: OrganizerHistogramComponent;
@@ -8,14 +8,68 @@ describe('OrganizerHistogramComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [OrganizerHistogramComponent]
-    });
+      declarations: [OrganizerHistogramComponent],
+      imports: [NgChartsModule],
+    }).compileComponents();
     fixture = TestBed.createComponent(OrganizerHistogramComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    component.finalResponses = new Map<string, number>([
+      ['Paris', 15],
+      ['Dollar', 5],
+      ['Pound', 8],
+      ['52', 4],
+      ['50', 11],
+      ['Oxygen', 12],
+      ['Mars', 14],
+    ]);
+
+    component.changingResponses = new Map<string, number>([
+      ['Paris', 15],
+      ['Dollar', 5],
+      ['Pound', 8],
+      ['52', 4],
+      ['50', 11],
+      ['Oxygen', 12],
+      ['Mars', 14],
+    ]);
+    component.valueOfResponses = new Map<string, boolean>([
+      ['Paris', true],
+      ['Dollar', false],
+      ['Pound', true],
+      ['52', false],
+      ['50', true],
+      ['Oxygen', true],
+      ['Mars', true],
+    ]);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should change barChartData on change', () => {
+    fixture.detectChanges();
+    component.finalResponses = new Map([
+      ['Paris', 20],
+      ['Dollar', 30],
+      ['Trick 1', 14],
+      ['52', 13],
+      ['50', 12],
+      ['Oxygen', 45],
+      ['Mars', 29],
+    ]);
+    component.changingResponses = new Map<string, number>([
+      ['Paris', 15],
+      ['Dollar', 5],
+      ['Pound', 8],
+      ['52', 4],
+      ['50', 11],
+      ['Trick 2', 12],
+      ['Mars', 14],
+    ]);
+    component.ngOnChanges();
+    expect(component.barChartData.labels).toEqual(['Paris', 'Dollar', 'Pound', '52', '50', 'Oxygen', 'Mars']);
+    expect(component.barChartData.datasets[0].data).toEqual([20, 30, 14, 13, 12, 45, 29]);
   });
 });
--- client/src/app/components/organizer-histogram/organizer-histogram.component.ts
+++ client/src/app/components/organizer-histogram/organizer-histogram.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-organizer-histogram',
   templateUrl: './organizer-histogram.component.html',
-  styleUrls: ['./organizer-histogram.component.scss']
+  styleUrls: ['./organizer-histogram.component.scss'],
 })
-export class OrganizerHistogramComponent {
+export class OrganizerHistogramComponent implements OnChanges {
+  @Input() finalResponses: Map<string, number>;
+  @Input() changingResponses: Map<string, number>;
+  @Input() valueOfResponses: Map<string, boolean>;
+  barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  };
+  barChartType: ChartType = 'bar';
+  barChartData: ChartData<'bar'>;
 
+  ngOnChanges() {
+    const labels = Array.from(this.valueOfResponses.keys());
+    const finalChoicesData = [];
+    const changingResponsesData = [];
+
+    for (const key of labels) {
+      finalChoicesData.push(this.finalResponses.get(key) || 0);
+      changingResponsesData.push(this.changingResponses.get(key) || 0);
+    }
+
+    const finalResponseColors = labels.map(
+        (label) => this.valueOfResponses.get(label) ? 'green' : 'red',
+    );
+
+    const changingResponseColors = labels.map(
+        (label) => (this.valueOfResponses.get(label) ? 'lightgreen' : 'pink'), // Lighter shades for changing responses
+    );
+
+    this.barChartData = {
+      labels,
+      datasets: [
+        {
+          data: finalChoicesData,
+          backgroundColor: finalResponseColors,
+        },
+        {
+          data: changingResponsesData,
+          backgroundColor: changingResponseColors,
+        },
+      ],
+    };
+  }
 }
