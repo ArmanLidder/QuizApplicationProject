4bde1f00c4973625db082973b1359718de8f57b9
Rayan Ait Abdesselam <rayan.ait-abdesselam@polymtl.ca>
2023-11-06 23:19:18 -0500

    fix: fixed bug where player score is not updated when end question from removal

--- server/app/services/socket-manager.service/socket-manager.service.ts
+++ server/app/services/socket-manager.service/socket-manager.service.ts
@@ -86,6 +86,9 @@ export class SocketManager {
                         } else if (game.playersAnswers.size === game.players.size) {
                             this.roomManager.getGameByRoomId(roomId).updateScores();
                             this.roomManager.clearRoomTimer(roomId);
+                            this.roomManager.getRoomById(roomId).players.forEach((socketId, username) => {
+                                if (username !== 'Organisateur') this.sio.to(socketId).emit('end question');
+                            });
                             this.sio.to(String(roomId)).emit('end question from removal');
                         }
                     }
