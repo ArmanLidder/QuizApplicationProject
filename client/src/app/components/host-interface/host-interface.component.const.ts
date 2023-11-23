baca33c33c159419fe0c5ccf4707a59c52a6c7e2
hb.96.montreal <hb.96.montreal@gmail.com>
2023-11-23 01:09:19 -0500

    feature/add TransportStatsFormat type

--- client/src/app/components/host-interface/host-interface.component.const.ts
+++ client/src/app/components/host-interface/host-interface.component.const.ts
@@ -1,1 +1,5 @@
+import {QuizQuestion} from "@common/interfaces/quiz.interface";
+
 export const PLAYER_NOT_FOUND_INDEX = -1;
+export type TransportStatsFormat = [[string,boolean][],[string, number][],QuizQuestion][];
+
