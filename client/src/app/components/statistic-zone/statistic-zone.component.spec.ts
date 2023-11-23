f4ad93212df4f1180dd91973e230c7163271923b
hb.96.montreal <hb.96.montreal@gmail.com>
2023-11-22 21:49:12 -0500

    test/statistic-zone almost done

--- client/src/app/components/statistic-zone/statistic-zone.component.spec.ts
+++ client/src/app/components/statistic-zone/statistic-zone.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { StatisticZoneComponent } from './statistic-zone.component';
+import {OrganizerHistogramComponent} from "@app/components/organizer-histogram/organizer-histogram.component";
+import {mockStats as mockSta} from "@app/components/statistic-zone/statistic-zone.component.const";
+import {NgChartsModule} from "ng2-charts";
 
 describe('StatisticZoneComponent', () => {
     let component: StatisticZoneComponent;
@@ -8,11 +10,24 @@ describe('StatisticZoneComponent', () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            declarations: [StatisticZoneComponent],
+            declarations: [StatisticZoneComponent, OrganizerHistogramComponent],
+            imports: [NgChartsModule]
         });
         fixture = TestBed.createComponent(StatisticZoneComponent);
         component = fixture.componentInstance;
+        component.gameStats = mockSta;
         fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should call setUpData when changes', () => {
+        const setUpDataSpy = spyOn<any>(component, 'setUpData');
+        component.ngOnInit();
+        fixture.detectChanges();
+        expect(setUpDataSpy).toHaveBeenCalled();
     });
 
     it('should create', () => {
