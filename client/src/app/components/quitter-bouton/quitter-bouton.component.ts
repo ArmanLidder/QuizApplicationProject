80514c71ad11c25272248584d29bcc166b453b3d
Mehdi <mehdi.el-harami@polymtl.ca>
2023-12-01 19:57:46 +0100

    refactor: some part of code

--- client/src/app/components/quitter-bouton/quitter-bouton.component.ts
+++ client/src/app/components/quitter-bouton/quitter-bouton.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
-import { ConfirmationDialogComponent } from '../confirmation-dialog/confirmation-dialog/confirmation-dialog.component';
+import { ConfirmationDialogComponent } from '@app/components/confirmation-dialog/confirmation-dialog.component';
 
 @Component({
     selector: 'app-quitter-button',
@@ -9,22 +9,21 @@ import { ConfirmationDialogComponent } from '../confirmation-dialog/confirmation
     styleUrls: ['./quitter-bouton.component.scss'],
 })
 export class QuitterButtonComponent {
-    constructor(private dialog: MatDialog, private router: Router) {}
+    constructor(
+        private dialog: MatDialog,
+        private router: Router,
+    ) {}
 
     openConfirmationDialog(): void {
         const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
             width: '300px',
-            data: { message: 'Etes-vous sur de vouloir quitter?' }
+            data: { message: 'Etes-vous sur de vouloir quitter?' },
         });
 
-        dialogRef.afterClosed().subscribe(result => {
+        dialogRef.afterClosed().subscribe((result) => {
             if (result) {
                 this.router.navigate(['./home']);
-            } else {
-                // Action to perform if canceled
-                console.log('User canceled.');
             }
         });
     }
-
 }
