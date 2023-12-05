0054ffadff66e812eb7dee47949f07c743ac9cf6
hb.96.montreal <hb.96.montreal@gmail.com>
2023-12-04 19:59:02 -0500

    final-refractor/code cleaning

--- client/src/app/components/game-history-list/game-history-list.component.ts
+++ client/src/app/components/game-history-list/game-history-list.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { GameHistoryService } from '@app/services/game-history.service/game-history.service';
 import { GameInfo } from '@common/interfaces/game-info.interface';
-import { DEFAULT_RADIX_VALUE } from '@common/constants/game-history-list.component.const';
+import { ARROW_DOWN, ARROW_UP, COLUMN_DATE, COLUMN_NAME, DEFAULT_RADIX_VALUE } from '@common/constants/game-history-list.component.const';
+
 @Component({
     selector: 'app-history-list',
     templateUrl: './game-history-list.component.html',
@@ -9,11 +10,11 @@ import { DEFAULT_RADIX_VALUE } from '@common/constants/game-history-list.compone
 })
 export class GameHistoryListComponent implements OnInit {
     games: GameInfo[] = [];
-    isNameAscendingOrder = false;
-    isDateAscendingOrder = false;
-    isNameIconClicked = false;
-    isDateIconClicked = false;
-    constructor(public gameHistoryServices: GameHistoryService) {}
+    isNameIconClicked: boolean = false;
+    isDateIconClicked: boolean = false;
+    private isNameAscendingOrder = false;
+    private isDateAscendingOrder = false;
+    constructor(private gameHistoryServices: GameHistoryService) {}
 
     ngOnInit(): void {
         this.getAllGames();
@@ -22,7 +23,7 @@ export class GameHistoryListComponent implements OnInit {
     getAllGames() {
         this.gameHistoryServices.getAll().subscribe((res) => {
             this.games = res;
-            this.sortGameName();
+            this.sort(COLUMN_NAME);
         });
     }
 
@@ -31,31 +32,38 @@ export class GameHistoryListComponent implements OnInit {
         this.games = [];
     }
 
-    sortGameName() {
-        this.isNameAscendingOrder = !this.isNameAscendingOrder;
-        this.isNameIconClicked = true;
-        this.isDateIconClicked = false;
-        this.games.sort((a, b) => {
-            const nameA = a.gameName.toLowerCase();
-            const nameB = b.gameName.toLowerCase();
-            return this.isNameAscendingOrder ? nameA.localeCompare(nameB) : nameB.localeCompare(nameA);
-        });
-    }
-
-    sortDate() {
-        this.isDateAscendingOrder = !this.isDateAscendingOrder;
-        this.isDateIconClicked = true;
-        this.isNameIconClicked = false;
-        this.games.sort((a, b) => {
-            const dateA = this.convertDateToNumber(a.startTime);
-            const dateB = this.convertDateToNumber(b.startTime);
-            return this.isDateAscendingOrder ? dateA - dateB : dateB - dateA;
-        });
+    sort(option: string) {
+        this.changeOrder(option);
+        this.isNameIconClicked = option === COLUMN_NAME;
+        this.isDateIconClicked = option === COLUMN_DATE;
+        if (option === COLUMN_NAME) this.sortGameName();
+        else if (option === COLUMN_DATE) this.sortDate();
     }
 
     getSortIcon(column: string) {
-        if (column === 'name') return this.isNameAscendingOrder ? '▲' : '▼';
-        else return this.isDateAscendingOrder ? '▲' : '▼';
+        if (column === COLUMN_NAME) return this.isNameAscendingOrder ? ARROW_UP : ARROW_DOWN;
+        else return this.isDateAscendingOrder ? ARROW_UP : ARROW_DOWN;
+    }
+
+    private sortDate() {
+        this.games.sort((firstGame, secondGame) => {
+            const firstDate = this.convertDateToNumber(firstGame.startTime);
+            const secondDate = this.convertDateToNumber(secondGame.startTime);
+            return this.isDateAscendingOrder ? firstDate - secondDate : secondDate - firstDate;
+        });
+    }
+
+    private sortGameName() {
+        this.games.sort((firstGame, secondGame) => {
+            const firstGameName = firstGame.gameName.toLowerCase();
+            const secondGameName = secondGame.gameName.toLowerCase();
+            return this.isNameAscendingOrder ? firstGameName.localeCompare(secondGameName) : secondGameName.localeCompare(firstGameName);
+        });
+    }
+
+    private changeOrder(option: string) {
+        if (option === COLUMN_NAME) this.isNameAscendingOrder = !this.isNameAscendingOrder;
+        else if (option === COLUMN_DATE) this.isDateAscendingOrder = !this.isDateAscendingOrder;
     }
 
     private convertDateToNumber(dateString: string) {
